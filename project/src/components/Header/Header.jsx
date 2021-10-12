121411032a0c670efe2cd98cd001ce7765b99dc1
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-12 18:22:12 -0400

    October 12, 2021 6:22 PM

--- project/src/components/Header/Header.jsx
+++ project/src/components/Header/Header.jsx
@@ -1,10 +1,26 @@
-export default function Header() {
-    return(
-        <div className="header">
-            <h1 className="">Welcome to Wonderland!</h1>
-            <button onClick={() => {
+import { Component } from 'react';
 
-            }}>Click Me!</button>
-        </div>
-    )
+export default class Header extends Component {
+   constructor(){
+       super();
+
+       this.state = {
+           red: true
+       }
+   }
+
+   changeColor(){
+       this.setState({red: !this.state.red})
+   }
+   
+    render() {
+        let head_class = this.state.red ? "redBkgd" : "blueBkgd";
+
+        return(
+            <div className={head_class}>
+                <h1 className="">Welcome to Wonderland!</h1>
+                <button onClick={this.changeColor.bind(this)}>Click Me!</button>
+            </div>
+        )
+    }
 }
