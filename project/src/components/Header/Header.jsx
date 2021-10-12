27c89dc922997c3af8fdc2553adfa4bc81431277
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-12 18:12:10 -0400

    October 12, 2021 6:12 PM

--- project/src/components/Header/Header.jsx
+++ project/src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
 export default function Header() {
     return(
         <div className="header">
-            
+            <h1 className="">Welcome to Wonderland!</h1>
+            <button onClick={() => {
+
+            }}>Click Me!</button>
         </div>
     )
 }
