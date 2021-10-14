a297d46a1f36daf76e837d5d153b31ad6f7f765b
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-14 13:59:42 -0400

    October 14, 2021 1:59 PM

--- project/src/App.js
+++ project/src/App.js
@@ -1,5 +1,11 @@
 import './App.css';
 
+const profile = {
+  name: 'Sarah Geraci',
+  username: 'Hally255',
+  about: 'Student at University of Central Florida',
+  likes: ''
+}
 
 function App() {
   return (
