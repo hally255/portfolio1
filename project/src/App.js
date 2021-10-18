c7d8142ed461cb6c2806d751c8b5749525b478d4
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-18 16:05:06 -0400

    October 18, 2021 4:05 PM

--- project/src/App.js
+++ project/src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import './components/Chat/'
+import './components/Profile/Profile.jsx'
+import Profile from './components/Profile/Profile.jsx';
 
 const profile = {
-  name: '',
-  username: '',
-  about: '',
-  likes: '',
-  dislikes: ''
+  name: 'Sarah Geraci',
+  username: 'Hally255',
+  about: 'Student at University of Central Florida',
+  likes: 'Video Games, Reading, Art, and Reptiles',
+  dislikes: 'Homework, Test, Exams, and Rude People'
 }
 
 function App() {
   return (
     <div className="App">
+      <Profile />
     </div>
   );
 }
