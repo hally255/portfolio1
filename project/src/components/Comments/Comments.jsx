4c65654b5641b489ced6b489606205c51521bf6b
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-18 17:16:09 -0400

    October 18, 2021 5:16 PM

---
+++ project/src/components/Comments/Comments.jsx
@@ -0,0 +1,11 @@
+export default function Comments() {
+
+    return (
+        <div className="comments">
+            <form>
+            <input type="text" placeholde="Comments..." />
+            <input type="submit" value="submit" />
+            </form>
+        </div>
+    )
+}
