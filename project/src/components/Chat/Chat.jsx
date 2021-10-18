fdce99dc51d9929d620129392150c273a8c44673
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-18 16:01:13 -0400

    October 18, 2021 4:01 PM

--- project/src/components/Chat/Chat.jsx
+++ project/src/components/Chat/Chat.jsx
@@ -1,9 +1,18 @@
-export default function Chat() {
-    return (
-        <div className="chatBox">
-            <input type="text" onChange={() => {
-                
-            }} />
-        </div>
-    )
+import { Component } from "react";
+
+export default class Chat extends Component  {
+    constructor(props) {
+        super(props);
+        this.state ={value: ''}
+    }
+
+    render() {
+        return (
+            <div className="chatBox">
+                <input type="text" onChange={() => {
+    
+                }} />
+            </div>
+        )
+    }
 }
