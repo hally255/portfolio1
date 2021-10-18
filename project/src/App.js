1252681f44cad8aa1ce80e0245b5472b8d2bef02
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-18 16:36:55 -0400

    October 18, 2021 4:36 PM

--- project/src/App.js
+++ project/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Profile from './components/Profile/Profile.jsx';
 import './components/Profile/me.png'
+import Feed from './components/Feed/Feed';
 
 function App() {
   return (
     <div className="App">
       <Profile />
+      <Feed />
     </div>
   );
 }
--- project/src/components/Feed/Feed.jsx
+++ project/src/components/Feed/Feed.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react"
+
 export default function Feed(props) {
 
+    const [likes, setLikes] = useState(0);
 
     return (
         <div className="feed">
-            <h2></h2>
+            <h2>Look at my dog!!</h2>
+            <img src='./dog.JPG' alt='cute dog' />
         </div>
     )
 }
