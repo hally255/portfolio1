8b55d1ad57269fe87b489f042d614f537172bf50
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-18 16:43:11 -0400

    October 18, 2021 4:43 PM

--- project/src/components/Feed/Feed.jsx
+++ project/src/components/Feed/Feed.jsx
@@ -1,14 +1,15 @@
 import { useState } from "react";
-import './dog.JPG';
+import image from './dog.JPG';
+import './Feed.css';
 
 export default function Feed(props) {
 
-    const [likes, setLikes] = useState(0);
+    //const [likes, setLikes] = useState(0);
 
     return (
         <div className="feed">
             <h2>Look at my dog!!</h2>
-            <img src='dog.JPG' alt='cute dog' />
+            <img src={image} alt='cute dog' />
         </div>
     )
 }
