ee3ea32c8f90d0136a2b578986b3b74382403a5a
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-18 18:04:18 -0400

    October 18, 2021 6:04 PM

--- project/src/components/Comments/Comments.jsx
+++ project/src/components/Comments/Comments.jsx
@@ -2,17 +2,13 @@ import './Comments.css';
 
 export default function Comments() {
 
-    let handleSubmit = (e) => {
-        e.preventDefault();
-
-        return(<p>{e.target.value}</p>);
-    }
-
     return (
         <div className="comments">
-            <form onSubmit={handleSubmit}>
-            <input className="textBox" type="text" onSubmit="" />
-            <button className="enter" type="sumbit">Enter</button>
+            <form >
+            <input className="textBox" type="text" />
+            <button className="enter" onClick={(e) => {
+                return(<p>{e.target.value}</p>)
+            }}>Enter</button>
             </form>
         </div>
     )
