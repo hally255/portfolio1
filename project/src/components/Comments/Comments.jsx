e5da15d9b58f78be5ccf5a86285afd5c7aa58bf4
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-18 17:49:24 -0400

    October 18, 2021 5:49 PM

--- project/src/components/Comments/Comments.jsx
+++ project/src/components/Comments/Comments.jsx
@@ -5,19 +5,20 @@ export default function Comments() {
 
     const [comment, setComment] = useState([]);
 
-    const handleSubmit = (event) => {
-        event.preventDefault();
+    const addComment = () => {
+        setComment([...comment, 'Entry ${comment.length}'])
     }
 
     return (
         <div className="comments">
-            <form onSubmit={handleSubmit}>
+            <form>
             <input className="textBox" type="text" value={comment} onChange={(e) => {
                 setComment(e.target.value);
             }} />
-            <input className="enter" type="submit" value="Enter" />
+            <input className="enter" type="button" onClick={addComment} value="enter" />
             </form>
-            <div>{comment.map()}</div>
+            <div>{comment.map(entry => 
+                <div>{entry}</div>)}</div>
         </div>
     )
 }
