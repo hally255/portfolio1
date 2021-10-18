8bbcc79ef07ae4744b5e56b8e6c1469fdce3b482
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-18 17:39:20 -0400

    October 18, 2021 5:39 PM

--- project/src/components/Comments/Comments.jsx
+++ project/src/components/Comments/Comments.jsx
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import './Comments.css';
 
 export default function Comments() {
 
+    const [comment, setComment] = useState([]);
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        
+    }
+
     return (
         <div className="comments">
-            <form>
-            <input className="textBox" type="text" placeholde="Comments..." />
-            <input className="enter" type="submit" value="Enter" onSubmit={(c) => {
-                
+            <form onSubmit={handleSubmit}>
+            <input className="textBox" type="text" value={comment} onChange={(e) => {
+                setComment(e.target.value);
             }} />
+            <input className="enter" type="submit" value="Enter" />
             </form>
+
         </div>
     )
 }
