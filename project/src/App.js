fd698ffd41c5d10b14e050db64f1ad0c6d72d548
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-18 16:07:21 -0400

    October 18, 2021 4:07 PM

--- project/src/App.js
+++ project/src/App.js
@@ -2,14 +2,6 @@ import './App.css';
 import './components/Profile/Profile.jsx'
 import Profile from './components/Profile/Profile.jsx';
 
-const profile = {
-  name: 'Sarah Geraci',
-  username: 'Hally255',
-  about: 'Student at University of Central Florida',
-  likes: 'Video Games, Reading, Art, and Reptiles',
-  dislikes: 'Homework, Test, Exams, and Rude People'
-}
-
 function App() {
   return (
     <div className="App">
--- project/src/components/Profile/Profile.jsx
+++ project/src/components/Profile/Profile.jsx
@@ -1,16 +1,22 @@
-import { Component } from "react";
 import './Profile.css';
 
-export default class Profile extends Component {
+export default function Profile() {
+    const profile = {
+        name: 'Sarah Geraci',
+        username: 'Hally255',
+        about: 'Student at University of Central Florida',
+        likes: 'Video Games, Reading, Art, and Reptiles',
+        dislikes: 'Homework, Test, Exams, and Rude People'
+      }
 
-    render(){
         return(
             <div>
                 <div className="imgDiv">
                     <img src="" alt="user" />
                 </div>
-                <h1>{this.props.username}</h1>
+                <h1>{profile.username}</h1>
+                <h3>{profile.name}</h3>
+                <h3>{profile.about}</h3>
             </div>
         )
-    }
 }
