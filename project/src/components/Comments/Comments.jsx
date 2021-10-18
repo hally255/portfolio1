0af26dd44357b9345048c8c0c0a8f0624e3a4fdd
hally255 <37000292+hally255@users.noreply.github.com>
2021-10-18 18:12:43 -0400

    October 18, 2021 6:12 PM

--- project/src/components/Comments/Comments.jsx
+++ project/src/components/Comments/Comments.jsx
@@ -1,15 +1,17 @@
+import { useState } from 'react';
 import './Comments.css';
 
 export default function Comments() {
 
+    const [comment, setComment] = useState([]);
+
     return (
         <div className="comments">
-            <form >
-            <input className="textBox" type="text" />
-            <button className="enter" onClick={(e) => {
-                return(<p>{e.target.value}</p>)
-            }}>Enter</button>
-            </form>
+            <input className="textBox" type="text" onBlur={(e) => {
+                console.log(e.target.value);
+                setComment(comment.push(e.target.value));
+            }
+            } />
         </div>
     )
 }
